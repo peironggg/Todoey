2e62962b00708fe39e595b64454ec7dbe0e253e5
Wu Peirong <wupeirong@macbook-pro.local>
2018-01-28 19:58:17 +0800

    Use default to persist local data

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -12,8 +12,13 @@ class TodoListViewController: UITableViewController {
     
     var itemArray = ["Find People", "Buy Food", "Yell"]
 
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -58,6 +63,7 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
         }
         
